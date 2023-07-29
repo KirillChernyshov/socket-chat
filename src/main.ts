91ee3b0259cd6dba9744de929f1857220bec2914
Kirill Chernyshov <k.c.coom@gmail.com>
2023-07-29 23:58:52 +0300

    [add][dependencies] pinia

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import '@/style.css';
 import App from '@/App.vue';
 import router from '@/plugins/router';
 import vuetify from '@/plugins/vuetify';
-import { initEvents } from '@/api';
+
+const pinia = createPinia();
 
 createApp(App)
   .use(router)
   .use(vuetify)
+  .use(pinia)
   .mount('#app');
-
-initEvents();
